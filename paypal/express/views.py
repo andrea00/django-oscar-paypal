09e8ec2ec95963a91e886446d7ac784b82582a2d
Andrea Garbano <ag@doppiozero.to>
2018-09-28 12:59:00 +0200

    old views

--- paypal/express/views.py
+++ paypal/express/views.py
@@ -13,15 +13,13 @@ from django.http import HttpResponseRedirect
 from django.utils.http import urlencode
 from django.utils import six
 from django.utils.translation import ugettext_lazy as _
-from django.core.exceptions import ObjectDoesNotExist
 
 import oscar
 from oscar.apps.payment.exceptions import UnableToTakePayment
 from oscar.core.exceptions import ModuleNotFoundError
 from oscar.core.loading import get_class, get_model
 from oscar.apps.shipping.methods import FixedPrice, NoShippingRequired
-
-from home.utils import send_email_to_admin
+from oscar.apps.address.models import UserAddress
 
 from paypal.express.facade import (
     get_paypal_url, fetch_transaction_details, confirm_transaction)
@@ -34,15 +32,9 @@ from paypal.exceptions import PayPalError
 PaymentDetailsView = get_class('checkout.views', 'PaymentDetailsView')
 CheckoutSessionMixin = get_class('checkout.session', 'CheckoutSessionMixin')
 
-UnableToPlaceOrder = get_class('order.exceptions', 'UnableToPlaceOrder')
-OrderNumberGenerator = get_class('order.utils', 'OrderNumberGenerator')
-OrderCreator = get_class('order.utils', 'OrderCreator')
-BillingAddress = get_model('order', 'BillingAddress')
 ShippingAddress = get_model('order', 'ShippingAddress')
-UserAddress = get_model('address', 'UserAddress')
 Country = get_model('address', 'Country')
 Basket = get_model('basket', 'Basket')
-Order = get_model('order', 'Order')
 Repository = get_class('shipping.repository', 'Repository')
 Selector = get_class('partner.strategy', 'Selector')
 Source = get_model('payment', 'Source')
@@ -67,8 +59,6 @@ class RedirectView(CheckoutSessionMixin, RedirectView):
     # address.  This is False when redirecting to PayPal straight from the
     # basket page but True when redirecting from checkout.
     as_payment_method = False
-
-    _payment_sources = None
 
     def get_redirect_url(self, **kwargs):
         try:
@@ -103,8 +93,6 @@ class RedirectView(CheckoutSessionMixin, RedirectView):
             basket.freeze()
 
             logger.info("Basket #%s - redirecting to %s", basket.id, url)
-
-            self.submit(**self.build_submission())
 
             return url
 
@@ -141,8 +129,6 @@ class RedirectView(CheckoutSessionMixin, RedirectView):
             shipping_methods = Repository().get_shipping_methods(
                 user=user, basket=basket, request=self.request)
             params['shipping_methods'] = shipping_methods
-            # params['shipping_method'] = shipping_method
-            params['shipping_methods'] = []           
 
         if settings.DEBUG:
             # Determine the localserver's hostname to use when
@@ -161,285 +147,6 @@ class RedirectView(CheckoutSessionMixin, RedirectView):
         Return any additional PayPal parameters
         """
         return {}
-
-    #CREAZIONE ORDINE PRIMA DEL PAGAMENTO
-    def submit(self, user, basket, shipping_address, shipping_method,  # noqa (too complex (10))
-                   shipping_charge, billing_address, order_total,
-                   payment_kwargs=None, order_kwargs=None):
-            """
-            Submit a basket for order placement.
-            The process runs as follows:
-             * Generate an order number
-             * Freeze the basket so it cannot be modified any more (important when
-               redirecting the user to another site for payment as it prevents the
-               basket being manipulated during the payment process).
-             * Attempt to take payment for the order
-               - If payment is successful, place the order
-               - If a redirect is required (eg PayPal, 3DSecure), redirect
-               - If payment is unsuccessful, show an appropriate error message
-            :basket: The basket to submit.
-            :payment_kwargs: Additional kwargs to pass to the handle_payment
-                             method. It normally makes sense to pass form
-                             instances (rather than model instances) so that the
-                             forms can be re-rendered correctly if payment fails.
-            :order_kwargs: Additional kwargs to pass to the place_order method
-            """
-            if payment_kwargs is None:
-                payment_kwargs = {}
-            if order_kwargs is None:
-                order_kwargs = {}
-
-            # Taxes must be known at this point
-            assert basket.is_tax_known, (
-                "Basket tax must be set before a user can place an order")
-            assert shipping_charge.is_tax_known, (
-                "Shipping charge tax must be set before a user can place an order")
-
-            # We generate the order number first as this will be used
-            # in payment requests (ie before the order model has been
-            # created).  We also save it in the session for multi-stage
-            # checkouts (eg where we redirect to a 3rd party site and place
-            # the order on a different request).
-            order_number = OrderNumberGenerator().order_number(basket)
-            self.checkout_session.set_order_number(order_number)
-            logger.info("Order #%s: beginning submission process for basket #%d",
-                        order_number, basket.id)
-
-            # Freeze the basket so it cannot be manipulated while the customer is
-            # completing payment on a 3rd party site.  Also, store a reference to
-            # the basket in the session so that we know which basket to thaw if we
-            # get an unsuccessful payment response when redirecting to a 3rd party
-            # site.
-            basket.freeze()
-            self.checkout_session.set_submitted_basket(basket)
-
-            # We define a general error message for when an unanticipated payment
-            # error occurs.
-            error_msg = _("A problem occurred while processing payment for this "
-                          "order - no payment has been taken.  Please "
-                          "contact customer services if this problem persists")
-
-            #signals.pre_payment.send_robust(sender=self, view=self)
-
-            #try:
-            #   self.handle_payment(order_number, order_total, **payment_kwargs)
-            # except RedirectRequired as e:
-            #     # Redirect required (eg PayPal, 3DS)
-            #     logger.info("Order #%s: redirecting to %s", order_number, e.url)
-            #     return http.HttpResponseRedirect(e.url)
-            # except UnableToTakePayment as e:
-            #     # Something went wrong with payment but in an anticipated way.  Eg
-            #     # their bankcard has expired, wrong card number - that kind of
-            #     # thing. This type of exception is supposed to set a friendly error
-            #     # message that makes sense to the customer.
-            #     msg = six.text_type(e)
-            #     logger.warning(
-            #         "Order #%s: unable to take payment (%s) - restoring basket",
-            #         order_number, msg)
-            #     self.restore_frozen_basket()
-            #
-            #     # We assume that the details submitted on the payment details view
-            #     # were invalid (eg expired bankcard).
-            #     return self.render_payment_details(
-            #         self.request, error=msg, **payment_kwargs)
-            # except PaymentError as e:
-            #     # A general payment error - Something went wrong which wasn't
-            #     # anticipated.  Eg, the payment gateway is down (it happens), your
-            #     # credentials are wrong - that king of thing.
-            #     # It makes sense to configure the checkout logger to
-            #     # mail admins on an error as this issue warrants some further
-            #     # investigation.
-            #     msg = six.text_type(e)
-            #     logger.error("Order #%s: payment error (%s)", order_number, msg,
-            #                  exc_info=True)
-            #     self.restore_frozen_basket()
-            #     return self.render_preview(
-            #         self.request, error=error_msg, **payment_kwargs)
-            # except Exception as e:
-            #     # Unhandled exception - hopefully, you will only ever see this in
-            #     # development...
-            #     logger.error(
-            #         "Order #%s: unhandled exception while taking payment (%s)",
-            #         order_number, e, exc_info=True)
-            #     self.restore_frozen_basket()
-            #     return self.render_preview(
-            #         self.request, error=error_msg, **payment_kwargs)
-
-           #signals.post_payment.send_robust(sender=self, view=self)
-
-            # If all is ok with payment, try and place order
-            logger.info("Order #%s: payment successful, placing order",
-                        order_number)
-            try:
-                return self.handle_order_placement(
-                    order_number, user, basket, shipping_address, shipping_method,
-                    shipping_charge, billing_address, order_total, **order_kwargs)
-            except UnableToPlaceOrder as e:
-                # It's possible that something will go wrong while trying to
-                # actually place an order.  Not a good situation to be in as a
-                # payment transaction may already have taken place, but needs
-                # to be handled gracefully.
-                print e
-                msg = six.text_type(e)
-                logger.error("Order #%s: unable to place order - %s",
-                             order_number, msg, exc_info=True)
-                self.restore_frozen_basket()
-                #return self.render_preview(
-                #    self.request, error=msg, **payment_kwargs)
-
-    def handle_order_placement(self, order_number, user, basket,
-                               shipping_address, shipping_method,
-                               shipping_charge, billing_address, order_total,
-                               **kwargs):
-        """
-        Write out the order models and return the appropriate HTTP response
-        We deliberately pass the basket in here as the one tied to the request
-        isn't necessarily the correct one to use in placing the order.  This
-        can happen when a basket gets frozen.
-        """
-        order = Order.objects.filter(number=order_number)
-        if order:
-            try:
-                order = order[0]
-            except:
-                order = None
-        if not order:
-            order = self.place_order(
-                order_number=order_number, user=user, basket=basket,
-                shipping_address=shipping_address, shipping_method=shipping_method,
-                shipping_charge=shipping_charge, order_total=order_total,
-                billing_address=billing_address, **kwargs)
-
-        #basket.submit()
-        #self.checkout_session.flush()
-        if getattr(settings, 'ORDER_EMAIL_ALWAYS', False):
-            send_email_to_admin(order, ordine=True)
-        self.request.session['checkout_order_id'] = order.id
-        #return self.handle_successful_order(order)
-
-    def place_order(self, order_number, user, basket, shipping_address,
-                    shipping_method, shipping_charge, order_total,
-                    billing_address=None, **kwargs):
-        """
-        Writes the order out to the DB including the payment models
-        """
-        # Create saved shipping address instance from passed in unsaved
-        # instance
-        shipping_address = self.create_shipping_address(user, shipping_address)
-
-        # We pass the kwargs as they often include the billing address form
-        # which will be needed to save a billing address.
-        billing_address = self.create_billing_address(
-            user, billing_address, shipping_address, **kwargs)
-
-        if 'status' not in kwargs:
-            status = None #self.get_initial_order_status(basket)
-        else:
-            status = kwargs.pop('status')
-        print kwargs
-        if 'request' not in kwargs:
-            request = getattr(self, 'request', None)
-        else:
-            request = kwargs.pop('request')
-        print kwargs
-        order = OrderCreator().place_order(
-            user=user,
-            order_number=order_number,
-            basket=basket,
-            shipping_address=shipping_address,
-            shipping_method=shipping_method,
-            shipping_charge=shipping_charge,
-            total=order_total,
-            billing_address=billing_address,
-            status=status,
-            #request=request,
-            **kwargs)
-        self.save_payment_sources(order)
-
-        return order
-
-    def save_payment_sources(self, order):
-        """
-        Saves any payment sources used in this order.
-        When the payment sources are created, the order model does not exist
-        and so they need to have it set before saving.
-        """
-        if not self._payment_sources:
-            return
-        for source in self._payment_sources:
-            source.order = order
-            source.save()
-
-    def create_shipping_address(self, user, shipping_address):
-        """
-        Create and return the shipping address for the current order.
-        Compared to self.get_shipping_address(), ShippingAddress is saved and
-        makes sure that appropriate UserAddress exists.
-        """
-        # For an order that only contains items that don't require shipping we
-        # won't have a shipping address, so we have to check for it.
-        if not shipping_address:
-            return None
-        shipping_address.save()
-        if user.is_authenticated():
-            self.update_address_book(user, shipping_address)
-
-        return shipping_address
-
-    def create_billing_address(self, user, billing_address=None,
-                               shipping_address=None, **kwargs):
-        """
-        Saves any relevant billing data (eg a billing address).
-        """
-        if not billing_address:
-            return None
-        billing_address.save()
-        if user.is_authenticated():
-            self.update_address_book(user, billing_address)
-
-        return billing_address
-
-    def restore_frozen_basket(self):
-        """
-        Restores a frozen basket as the sole OPEN basket.  Note that this also
-        merges in any new products that have been added to a basket that has
-        been created while payment.
-        """
-        try:
-            fzn_basket = self.get_submitted_basket()
-        except Basket.DoesNotExist:
-            # Strange place.  The previous basket stored in the session does
-            # not exist.
-            pass
-        else:
-            fzn_basket.thaw()
-            if self.request.basket.id != fzn_basket.id:
-                fzn_basket.merge(self.request.basket)
-                # Use same strategy as current request basket
-                fzn_basket.strategy = self.request.basket.strategy
-
-        self.request.basket = fzn_basketa
-
-    def update_address_book(self, user, addr):
-            """
-            Update the user's address book based on the new shipping address
-            """
-            try:
-                user_addr = user.addresses.get(
-                    hash=addr.generate_hash())
-            except ObjectDoesNotExist:
-                # Create a new user address
-                user_addr = UserAddress(user=user)
-                addr.populate_alternative_model(user_addr)
-            try:
-                if isinstance(addr, ShippingAddress):
-                    user_addr.num_orders_as_shipping_address += 1
-                if isinstance(addr, BillingAddress):
-                    user_addr.num_orders_as_billing_address += 1
-            except Exception, e:
-                print e
-
-            user_addr.save()
 
 
 class CancelResponseView(RedirectView):
